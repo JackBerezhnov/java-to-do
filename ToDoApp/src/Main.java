60be7b97227823de288e7fd761e6269b08047193
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-05 22:05:45 -0700

    Added Input and Changed text for a button

--- ToDoApp/src/Main.java
+++ ToDoApp/src/Main.java
@@ -6,12 +6,15 @@ public class Main {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		
-		JButton button = new JButton("Click");
+		JButton button = new JButton("Add Task");
 		JPanel panel = new JPanel();
+		JTextField input = new JTextField();
 		
 		button.setBounds(150, 200, 220, 50);
+		input.setBounds(0, 0, 500, 100);
 		
 		panel.add(button);
+		panel.add(input);
 		
 		frame.add(panel);
 		
