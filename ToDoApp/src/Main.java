3804cdc526ef72c49f0aaf96ac5cc1f9167ce391
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-06 16:45:40 -0700

    Added a feature to add task in the list

--- ToDoApp/src/Main.java
+++ ToDoApp/src/Main.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -9,20 +11,33 @@ public class Main {
 		String tasks[] = {"Wash a car", "Work", "Sleep"};
 		
 		JFrame frame = new JFrame();
-		JButton button = new JButton("Add Task");
+		JButton addButton = new JButton("Add Task");
 		JPanel panel = new JPanel();
 		JTextField input = new JTextField();
-		JList listOfTasks = new JList(tasks);
+		DefaultListModel<String> taskListModel = new DefaultListModel<>();
+		JList<String> listOfTasks = new JList<>(taskListModel);
 		
-		button.setBounds(150, 200, 220, 50);
+		addButton.setBounds(150, 200, 220, 50);
 		Dimension preferredSize = new Dimension(300, 20);
 		input.setPreferredSize(preferredSize);
 			
 		panel.add(input);
-		panel.add(button);
-		panel.add(listOfTasks);
+		panel.add(addButton);
 		
-		frame.add(panel);
+		frame.add(panel, "North");
+		frame.add(new JScrollPane(listOfTasks), "Center");
+
+		addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String task = input.getText().trim();
+				if(!task.isEmpty()) {
+					taskListModel.addElement(task);
+					input.setText("");
+				}
+			}
+		});
+		
 		frame.setSize(500, 600);
 		frame.setVisible(true); 
 		
