73d65e207ac6059ba92646047ce8842a928175ab
Evgeny Berezhnov <33531079+JackBerezhnov@users.noreply.github.com>
2024-07-05 22:17:36 -0700

    Added a list of tasks

--- ToDoApp/src/Main.java
+++ ToDoApp/src/Main.java
@@ -5,18 +5,22 @@ import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
 		
+		String tasks[] = {"Wash a car", "Work", "Sleep"};
+		
+		JFrame frame = new JFrame();
 		JButton button = new JButton("Add Task");
 		JPanel panel = new JPanel();
 		JTextField input = new JTextField();
+		JList listOfTasks = new JList(tasks);
 		
 		button.setBounds(150, 200, 220, 50);
 		Dimension preferredSize = new Dimension(300, 20);
 		input.setPreferredSize(preferredSize);
-		
+			
 		panel.add(input);
 		panel.add(button);
+		panel.add(listOfTasks);
 		
 		frame.add(panel);
 		frame.setSize(500, 600);
